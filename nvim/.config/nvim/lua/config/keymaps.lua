5df5e750b6e258c5540152276d212db73ea627bd
Popplywop <jokobpopple96@gmail.com>
2024-12-17 15:34:23 -0600

    add terminal keymaps and autocmds

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -8,6 +8,26 @@ vim.keymap.set('i', '<C-c>', "<Esc>")
 vim.keymap.set('n', 'Q', '<nop>')
 vim.keymap.set('x', '<leader>p', [["_dP]])
 
+------ Terminal -------
+vim.api.nvim_create_autocmd('TermOpen', {
+  group = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
+  callback = function()
+    vim.opt.number = false
+    vim.opt.relativenumber = false
+  end,
+})
+
+local job_id = 0
+vim.keymap.set('n', '<leader>st', function()
+  vim.cmd.vnew()
+  vim.cmd.term()
+  vim.cmd.wincmd('J')
+  vim.api.nvim_win_set_height(0, 10)
+
+  job_id = vim.bo.channel
+end)
+
+vim.keymap.set('t', '<C-t>', '<C-\\><C-n>', { noremap = true, silent = true })
 
 -- inlay hints
 vim.keymap.set('n', '<leader>H', function()
@@ -17,7 +37,8 @@ end, { desc = 'Show inlay hints' })
 ------- EDITING FILES --------
 
 -- Very specific to editing my episode_info text files used to rename .mkv files that i take from my dvds
-vim.keymap.set('n', '<leader>eei', [[:%s/e01/\=printf('e%02d', line('.'))/g<CR>]], { desc = 'Edit episode info text file' })
+vim.keymap.set('n', '<leader>eei', [[:%s/e01/\=printf('e%02d', line('.'))/g<CR>]],
+  { desc = 'Edit episode info text file' })
 
 -- Edit neovim config
 vim.keymap.set('n', '<leader>ec', '<cmd>e ~/.dotfiles/nvim/.config/nvim/init.lua<CR>', { desc = 'Edit config files' })

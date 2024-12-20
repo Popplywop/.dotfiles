e02c450068bb2c0fbf5d84acb2a002690e80e612
Jakob Popple <jpopple@pdinet.com>
2024-12-20 10:02:16 -0600

    fix which key for terminal

--- nvim/.config/nvim/lua/plugins/which-key.lua
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -12,14 +12,14 @@ return {
         { "<leader>c_", hidden = true },
         { "<leader>s",  group = "[S]earch" },
         { "<leader>s_", hidden = true },
-        { "<leader>t",  group = "[T]est" },
-        { "<leader>t_", hidden = true },
         { "<leader>w",  group = "[W]orkspace" },
         { "<leader>w_", hidden = true },
         { "<leader>o", group = "[O]mnisharp" },
         { "<leader>o_", hidden = true },
         { "<leader>e", group = "[E]dit" },
-        { "<leader>e_", hidden = true }
+        { "<leader>e_", hidden = true },
+        { "<leader>t", group = "[T]erminal" },
+        { "<leader>t_", hidden = true }
       }
     end,
   },

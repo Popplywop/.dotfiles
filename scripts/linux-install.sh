afb1667c0c13d8a80cca2b7b25a32493acad3ad4
Popplywop <jakob_popple@outlook.com>
2025-05-11 21:02:03 -0500

    remove useless script

--- scripts/linux-install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-for folder in ~/.dotfiles/*/; do
-  if [ -d "$folder" ]; then
-    folder_name=$(basename "$folder")
-    echo "Running stow for $folder_name"
-    stow "$folder_name"
-  fi
-done

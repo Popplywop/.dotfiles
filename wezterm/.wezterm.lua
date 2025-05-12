f899ddac65a42b799169567d2d4a51dc3bd85b4e
Popplywop <jakob_popple@outlook.com>
2025-05-11 20:59:46 -0500

    add hyprland, update wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -10,7 +10,7 @@ if is_windows() then
   config.default_prog = { 'pwsh.exe' }
 else
   config.default_prog = { 'bash' }
-  config.enable_wayland = true
+  config.enable_wayland = false
 end
 
 config.enable_tab_bar = true
@@ -27,22 +27,7 @@ config.font_size = 11.5
 
 local opacity = 0.7
 
--- powershell opacity is annoying so if on windows just dont set the background opacity
-if not is_windows() then
-  config.window_background_opacity = opacity
-end
-
 config.window_decorations = 'RESIZE'
-
-wezterm.on("toggle-opacity", function(window, pane)
-  local overrides = window:get_config_overrides() or {}
-  if not overrides.window_background_opacity then
-    overrides.window_background_opacity = 1.0
-  else
-    overrides.window_background_opacity = nil
-  end
-  window:set_config_overrides(overrides)
-end)
 
 config.leader = { key = 'a', mods = 'CTRL' }
 
@@ -51,11 +36,6 @@ config.keys = {
     key = 'w',
     mods = 'LEADER',
     action = act.CloseCurrentTab { confirm = true }
-  },
-  {
-    key = "O",
-    mods = "CTRL|SHIFT",
-    action = act.EmitEvent("toggle-opacity"),
   },
   {
     key = 'E',
